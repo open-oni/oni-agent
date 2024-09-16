9d0f827bc564d54f6fe127ecb408d79b6c1ed1e3
Jeremy Echols <jechols@uoregon.edu>
2024-09-16 12:20:58 -0700

    job: store metadata when starting jobs

--- cmd/agent/job.go
+++ cmd/agent/job.go
@@ -2,17 +2,22 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"strings"
+	"time"
 )
 
 type job struct {
 	command     *exec.Cmd
+	startedAt   time.Time
+	completedAt time.Time
 	err         error
 	stdout      bytes.Buffer
 	stderr      bytes.Buffer
+	pid         int
 }
 
 func newJob(args ...string) *job {
@@ -53,6 +58,43 @@ func newJob(args ...string) *job {
 // Run wraps exec.Cmd.Run, starting the job and waiting for it to finish, while
 // populating data within the job as needed
 func (j *job) Run() error {
-	j.err = j.command.Run()
+	j.Start()
+	if j.err == nil {
+		j.Wait()
+	}
+
 	return j.err
+}
+
+// Run wraps exec.Cmd.Start, setting the start time and kicking off the command
+// without waiting for it to finish. After calling Start, Wait must then be
+// called to let the command finish and release resources.
+func (j *job) Start() error {
+	j.err = j.command.Start()
+	if j.err != nil {
+		return j.err
+	}
+
+	j.startedAt = time.Now()
+	j.pid = j.command.Process.Pid
+	return nil
+}
+
+// Wait wraps exec.Cmd.Wait, waiting for the command to exit and various stream
+// copying to complete, setting the completed time if successful.
+func (j *job) Wait() error {
+	if j.err != nil {
+		return fmt.Errorf("waiting for job completion: cannot start due to previous error: %w", j.err)
+	}
+	if j.startedAt.IsZero() {
+		return fmt.Errorf("waiting for job completion: Start must first be called")
+	}
+
+	j.err = j.command.Wait()
+	if j.err != nil {
+		return j.err
+	}
+
+	j.completedAt = time.Now()
+	return nil
 }
